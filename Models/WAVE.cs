e1a37f5bf60f414d2f198ebab8d5d55245ce7899
Kanwarjaspreet Singh Jaura <jessejaura@gmail.com>
2021-06-26 19:35:22 -0400

    Initial Release

---
+++ Models/WAVE.cs
@@ -0,0 +1,16 @@
+﻿namespace WAVUtils.Models
+{
+    public class WAVE : RIFF 
+    { 
+        public Fmt fmt { get; set; }
+        
+        public string subchunk2ID { get; set; }
+        
+        public int subchunk2Size { get; set; }
+        
+        public int[,] samplesByChannel { get; set; }
+        
+        // Normalized from to range of 0.0f to 1.0f
+        public float[,] normalizedSamplesByChannel { get; set; }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # WAVUtils
-Converts WAV File to C# Objects
+Converts WAV File to C# Objects using BinaryReader
+
+Currently supports 8-bit and 16-bit WAV files. Also normalizes samples to floats with a range of 0 to 1.
+
